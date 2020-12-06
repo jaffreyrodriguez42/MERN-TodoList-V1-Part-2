757a96dfb083f6f474c4a87759a051bfda4677b5
Jaf <jaffreyrodriguez42@gmail.com>
2020-12-07 00:02:13 +0800

    Rendering database items in the todolist app

---
+++ app.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/todolistDB", { useUnifiedTopology: true, useNewUrlParser: true });
+
+const itemsSchema = ({
+  name: String
+});
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+  name: "Eat Dinner"
+});
+
+const item2 = new Item({
+  name: "Drink Milk"
+});
+
+const item3 = new Item({
+  name: "Watch TV"
+});
+
+const defaultItems = [item1, item2, item3];
+
+app.get("/", function(req, res) {
+  Item.find({}, function(err, items){
+    if(items.length === 0){
+      Item.insertMany(defaultItems, function(err){
+      if(err){
+        console.log(err);
+      }else{
+        console.log("Successfully added many!");
+      }   
+      });
+      res.redirect("/");
+    }else{
+      res.render("list", {listTitle: "Today", newListItems: items});
+    }
+  });
+  
+});
+
+app.post("/", function(req, res){
+
+  const item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function(req,res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.get("/about", function(req, res){
+  res.render("about");
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
